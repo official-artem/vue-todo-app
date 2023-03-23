b15ec32409a164ae3518d2e30d94a0bce46f4a0a
Artem Hrechuk <grechuck.artem.ag@gmail.com>
2023-03-23 18:10:36 +0200

    add handle on click

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
+<script>
+import todos from './data/todos';
+
+export default {
+  data() {
+    return {
+      todos,
+    }
+  },
+  mounted() {
+    console.log(this.todos);
+  },
+  computed: {
+    activeTodos() {
+      return this.todos.filter(todo => !todo.completed)
+    }
+  }
+}
+</script>
+
 <template>
   <div class="todoapp">
       <h1 class="todoapp__title">todos</h1>
 
       <div class="todoapp__content">
         <header class="todoapp__header">
-          <button type="button" class="todoapp__toggle-all active" />
+          <button type="button" class="todoapp__toggle-all" :class="{ active: !activeTodos.length}" />
 
           <form>
             <input
@@ -16,51 +36,21 @@
         </header>
 
         <section class="todoapp__main">
-          <div class="todo completed">
+          <div 
+            v-for="todo, index of todos" 
+            :key="todo.id"
+            class="todo"
+            :class="{ completed: todo.completed }"
+          >
             <label class="todo__status-label">
               <input
                 type="checkbox"
                 class="todo__status"
-                checked
+                v-model="todo.completed"
               />
             </label>
 
-            <span class="todo__title">Completed Todo</span>
-
-            <button type="button" class="todo__remove">×</button>
-
-            <div class="modal overlay">
-              <div class="modal-background has-background-white-ter" />
-              <div class="loader" />
-            </div>
-          </div>
-
-          <div class="todo">
-            <label class="todo__status-label">
-              <input
-                type="checkbox"
-                class="todo__status"
-              />
-            </label>
-
-            <span class="todo__title">Not Completed Todo</span>
-            <button type="button" class="todo__remove">×</button>
-
-            <div class="modal overlay">
-              <div class="modal-background has-background-white-ter" />
-              <div class="loader" />
-            </div>
-          </div>
-
-          <div class="todo">
-            <label class="todo__status-label">
-              <input
-                type="checkbox"
-                class="todo__status"
-              />
-            </label>
-
-            <form>
+            <form v-if="false">
               <input
                 type="text"
                 class="todo__title-field"
@@ -69,21 +59,13 @@
               />
             </form>
 
-            <div class="modal overlay">
-              <div class="modal-background has-background-white-ter" />
-              <div class="loader" />
-            </div>
-          </div>
+            <template v-else>
+              <span class="todo__title">{{ todo.title }}</span>
 
-          <div class="todo">
-            <label class="todo__status-label">
-              <input type="checkbox" class="todo__status" />
-            </label>
+              <button type="button" class="todo__remove" @click="todos.splice(index, 1)">×</button>
+            </template>
 
-            <span class="todo__title">Todo is being saved now</span>
-            <button type="button" class="todo__remove">×</button>
-
-            <div class="modal overlay is-active">
+            <div class="modal overlay" :class="{ 'is-active': false }">
               <div class="modal-background has-background-white-ter" />
               <div class="loader" />
             </div>
@@ -92,7 +74,7 @@
 
         <footer class="todoapp__footer">
           <span class="todo-count">
-            3 items left
+            {{ activeTodos.length }} items left
           </span>
 
           <nav class="filter">
@@ -109,7 +91,7 @@
             </a>
           </nav>
 
-          <button type="button" class="todoapp__clear-completed">
+          <button v-if="activeTodos.length > 0" type="button" class="todoapp__clear-completed">
             Clear completed
           </button>
         </footer>
